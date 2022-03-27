c38962e135f80d1a152c0fdbb826734e1179abed
sabrina <yhssayah.kosu@gmail.com>
2022-03-27 20:18:43 +0900

    Finish layout and routes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,9 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
+  # test環境でもApplicationヘルパーを使えるようにする
+  include ApplicationHelper
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
